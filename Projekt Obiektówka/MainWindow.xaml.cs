f30665bd22c4093ddff870fe4c2e7e9fa7050d65
TheZetClaudio <zeter54.klaudiusz@gmail.com>
2023-07-09 21:50:22 +0200

    ViewModel Updata

--- Projekt Obiektówka/MainWindow.xaml.cs
+++ Projekt Obiektówka/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Projekt_Obiektówka.ViewsModels;
+﻿using ControlzEx.Standard;
 using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
@@ -14,58 +14,151 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data; //< DataBase Add>
+using Projekt_Obiektówka.ViewModels;
 
 namespace Projekt_Obiektówka
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
-            TextResult.Text = string.Empty;
-            Math.Text = string.Empty;
+            Math.Text = "0";
         }
-
+        #region Button_Click
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            TextResult.Text = string.Empty;
-
             var button = sender as Button;
             var currentNumber = button.Content;
             Math.Text += currentNumber;
         }
+        #endregion
 
-
-
-
+        #region Button_Clear
         private void Button_Clear(object sender, RoutedEventArgs e)
-        {
-
+        {  
+            Math.Text = "0";
         }
+        #endregion
+
+        #region Button_Result
         private void Button_Result(object sender, RoutedEventArgs e)
         {
+            var operation = Math.Text;
 
+            if(operation.Contains('+'))
+            {
+                var element = operation.Split('+');
+                var result = double.Parse(element[0]) + double.Parse(element[1]);
+                Math.Text = result.ToString();
+            }
+
+            if (operation.Contains('-'))
+            {
+                var element = operation.Split('-');
+                var result = double.Parse(element[0]) - double.Parse(element[1]);
+                Math.Text = result.ToString();
+            }
+
+            if (operation.Contains('*'))
+            {
+                var element = operation.Split('*');
+                var result = double.Parse(element[0]) * double.Parse(element[1]);
+                Math.Text = result.ToString();
+            }
+
+            if (operation.Contains('/'))
+            {
+                var element = operation.Split('/');
+                var result = double.Parse(element[0]) / double.Parse(element[1]);
+                Math.Text = result.ToString();
+            }
         }
+        #endregion
+
+        #region AllOperation
+        private bool ContainstOperation(string operation)
+        {
+            return operation.Contains("+") || operation.Contains("-") || operation.Contains("*") || operation.Contains("/");
+        }
+
         private void Button_Add(object sender, RoutedEventArgs e)
         {
+            var operation = Math.Text;
 
+
+            if (ContainstOperation(operation))
+            {
+                Math.Text = Math.Text.Replace("-", "+");
+                Math.Text = Math.Text.Replace("*", "+");
+                Math.Text = Math.Text.Replace("/", "+");
+            }
+            else
+            {
+                Math.Text += "+";
+            }
         }
         private void Button_Mult(object sender, RoutedEventArgs e)
         {
+            var operation = Math.Text;
 
+            if (ContainstOperation(operation))
+            {
+                Math.Text = Math.Text.Replace("+", "*");
+                Math.Text = Math.Text.Replace("-", "*");
+                Math.Text = Math.Text.Replace("/", "*");
+            }
+            else
+            {
+                Math.Text += "*";
+            }
         }
         private void Button_Minus(object sender, RoutedEventArgs e)
         {
+            var operation = Math.Text;
 
+            if (ContainstOperation(operation))
+            {
+                Math.Text = Math.Text.Replace("+", "-");
+                Math.Text = Math.Text.Replace("*", "-");
+                Math.Text = Math.Text.Replace("/", "-");
+            }
+            else
+            {
+                Math.Text += "-";
+            }
         }
         private void Button_Div(object sender, RoutedEventArgs e)
+        {
+            var operation = Math.Text;
+
+            if (ContainstOperation(operation))
+            {
+                Math.Text = Math.Text.Replace("-", "/");
+                Math.Text = Math.Text.Replace("*", "/");
+                Math.Text = Math.Text.Replace("+", "/");
+            }
+            else
+            {
+                Math.Text += "/";
+            }
+        }
+        #endregion
+        
+        private void bd_Updata(string sURL, string sTitle)
         {
 
         }
 
+        private void History_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new HistoryViewModel();
+        }
+
+        private void Calculator_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = null;
+        }
     }
 }
